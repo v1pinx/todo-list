95dfb68677706ddc248b612c18064cbc1e90eb03
Its-Vipin <vkprajapati665@gmail.com>
2024-04-05 17:44:44 +0530

    views update

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ require('./db');
 const router = require('./routes/app');
 
 app.set('view engine', 'ejs');
+app.use(express.static('views')); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
 
-app.listen(3000, () => {
+app.listen(3020, () => {
     console.log("Server is running at 3000");
 })
